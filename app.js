26ff08fe310195cc160bcab64382c7c7c8a0be1e
Anthony Aichouche <anthony.aichouche@student.hogent.be>
2023-01-16 00:16:26 +0100

    zbub zbub

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.use(function (req, res, next) {
     }
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Expose-Headers", "set-cookie");
     next();
 });
 
